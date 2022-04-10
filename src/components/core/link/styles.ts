08e67ce4d3d1d9bf07ac621a9b6e4695a776bf59
pedro-oliveira <pedro_oliver13@hotmail.com>
2022-04-10 12:10:24 -0300

    feat: Create component link

---
+++ src/components/core/link/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+import * as C from 'styles/constants';
+
+interface LinkProps {
+  isActive: boolean;
+}
+
+const linkModifier = {
+  notActive: {
+    fontWeight: '400',
+    color: C.Gray
+  }
+};
+
+export const Wrapper = styled.a<LinkProps>`
+  font-weight: '700';
+  font-size: 2.2rem;
+  cursor: pointer;
+  transition: color 0.2s ease-in-out;
+
+  ${({ isActive }) => !isActive && linkModifier.notActive}
+
+  &:hover {
+    color: ${C.LightPurple};
+  }
+`;
