f721f887f58eb772ec42cd016a59afabd6ee3ad8
pedro-oliveira <pedro_oliver13@hotmail.com>
2022-04-10 20:41:25 -0300

    feat: Create component circular-button

---
+++ src/components/core/circular-button/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+import * as C from 'styles/constants';
+
+export const Wrapper = styled.a`
+  max-width: 4.4rem;
+  width: 100%;
+  height: 4.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  border: 2px solid ${C.White};
+  border-radius: 50%;
+
+  svg {
+    width: 2.2rem;
+  }
+`;
--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -2,3 +2,4 @@ export * from 'components/core/avatar';
 export * from 'components/core/link';
 export * from 'components/core/button';
 export * from 'components/core/button-brand';
+export * from 'components/core/circular-button';
