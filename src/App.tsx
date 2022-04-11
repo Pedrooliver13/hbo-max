1926a2c4f349711b87c2063b2dbe6b2fcded848d
Pedro Oliveira <pedro_oliver13@hotmail.com>
2022-04-10 21:44:19 -0300

    feat: card component (#8)
    * feat: Create component card
    
    * feat: Add storybook in component card
    
    * test: Add test in component card

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { CircularButton } from 'components/core';
+import { Card } from 'components/core';
 import GlobalStyled from 'styles/global';
 
 function App() {
   return (
     <>
       <GlobalStyled />
-      <CircularButton name="teste" />
-      <CircularButton name="teste" icon="play" />
-      <CircularButton name="teste" icon="check" />
+      <Card
+        name="Card"
+        image="https://images.moviesanywhere.com/a4d68b895ddea9e52e345e2f0c6838ab/423fc271-7a33-4155-92db-3c926d8c1af3.jpg"
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      />
     </>
   );
 }
---
+++ src/components/core/card/card.test.js
@@ -0,0 +1,64 @@
+import { mount } from '@cypress/react';
+
+import { Card } from '.';
+import GlobalStyled from 'styles/global';
+
+const testButtonBrand = () => {
+  const defaultRender = (props) => {
+    mount(
+      <>
+        <GlobalStyled />
+        <Card
+          name="test"
+          image="https://images.moviesanywhere.com/a4d68b895ddea9e52e345e2f0c6838ab/423fc271-7a33-4155-92db-3c926d8c1af3.jpg"
+          href="https://www.imdb.com/title/tt0111161/"
+          {...props}
+        />
+      </>
+    );
+  };
+
+  const getCard = () => {
+    return cy.get('[data-cy=test]');
+  };
+
+  return { defaultRender, getCard };
+};
+
+describe('Link Component', () => {
+  it('Should render', () => {
+    const { defaultRender, getCard } = testButtonBrand();
+
+    defaultRender();
+    getCard().should('be.visible');
+  });
+
+  it('Should have buttons', () => {
+    const { defaultRender, getCard } = testButtonBrand();
+
+    defaultRender();
+    getCard().get('[data-cy=play]');
+    getCard().get('[data-cy=my-list]');
+  });
+
+  it('Should have a href', () => {
+    const { defaultRender, getCard } = testButtonBrand();
+
+    defaultRender({ href: 'https://www.google.com' });
+    getCard()
+      .get('[data-cy=play]')
+      .should('have.attr', 'href', 'https://www.google.com');
+  });
+
+  it('Should have a image', () => {
+    const { defaultRender, getCard } = testButtonBrand();
+
+    defaultRender({
+      image:
+        'https://images.moviesanywhere.com/a4d68b895ddea9e52e345e2f0c6838ab/423fc271-7a33-4155-92db-3c926d8c1af3.jpg'
+    });
+    getCard()
+      .should('have.css', 'background-image')
+      .should('include', '423fc271-7a33-4155-92db-3c926d8c1af3.jpg');
+  });
+});
