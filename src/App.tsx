b2270e08b37ee09fb3b8bbaf342d74da6498a964
pedro-oliveira <pedro_oliver13@hotmail.com>
2022-04-13 20:38:56 -0300

    feat: Add new sizes to component card

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         name="Card"
         image="https://images.moviesanywhere.com/a4d68b895ddea9e52e345e2f0c6838ab/423fc271-7a33-4155-92db-3c926d8c1af3.jpg"
         href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        size="sm"
       />
     </>
   );
--- src/components/core/card/stories.tsx
+++ src/components/core/card/stories.tsx
@@ -10,7 +10,21 @@ export default {
     name: 'Card',
     image:
       'https://images.moviesanywhere.com/a4d68b895ddea9e52e345e2f0c6838ab/423fc271-7a33-4155-92db-3c926d8c1af3.jpg',
-    href: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    href: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    label: 'Card',
+    size: 'md',
+    type: 'series'
+  },
+  argTypes: {
+    image: {
+      control: {
+        type: 'select',
+        options: [
+          'https://images.moviesanywhere.com/a4d68b895ddea9e52e345e2f0c6838ab/423fc271-7a33-4155-92db-3c926d8c1af3.jpg',
+          'https://d5y9g7a5.rocketcdn.me/wp-content/uploads/2020/08/friends-curiosidades-sobre-uma-das-maiores-series-de-todos-os-tempos-1024x576.jpg'
+        ]
+      }
+    }
   }
 } as Meta;
 
